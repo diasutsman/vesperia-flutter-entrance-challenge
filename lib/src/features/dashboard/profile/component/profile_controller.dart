840f125dbd164ad1ff549921e8eccba4f7dcaeee
Dias Utsman <utsmand91@gmail.com>
2024-05-29 16:48:01 +0700

    7.	Implement ‘download file’ button in profile page to directly save file from this link to your device.

--- lib/src/features/dashboard/profile/component/profile_controller.dart
+++ lib/src/features/dashboard/profile/component/profile_controller.dart
@@ -4,6 +4,10 @@ import 'package:get/get.dart';
 import '../../../../../app/routes/route_name.dart';
 import '../../../../utils/networking_util.dart';
 import '../../../../widgets/snackbar_widget.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 
 class ProfileController extends GetxController {
   final UserRepository _userRepository;
@@ -65,7 +69,49 @@ class ProfileController extends GetxController {
     await _userRepository.testUnauthenticated();
   }
 
-  onDownloadFileClick() async {}
+  void onDownloadFileClick() async {
+    final dio = Dio();
+    const url = 'https://www.tutorialspoint.com/flutter/flutter_tutorial.pdf';
+
+    // Request storage permissions
+    if (await _requestPermissions()) {
+      try {
+        final directory = await getDownloadsDirectory();
+        if (directory == null) {
+          SnackbarWidget.showFailedSnackbar(
+              'Unable to get downloads directory');
+          return;
+        }
+
+        final filePath = '${directory.path}/flutter_tutorial.pdf';
+        final response = await dio.download(url, filePath);
+
+        if (response.statusCode == 200) {
+          SnackbarWidget.showSuccessSnackbar('Download completed: $filePath');
+        } else {
+          SnackbarWidget.showFailedSnackbar('Download failed');
+        }
+      } catch (error) {
+        SnackbarWidget.showFailedSnackbar('Error: ${error.toString()}');
+      }
+    } else {
+      SnackbarWidget.showFailedSnackbar('Permission denied');
+    }
+  }
+
+  Future<bool> _requestPermissions() async {
+    final status = await Permission.storage.request();
+    return status.isGranted;
+  }
+
+  Future<Directory?> getDownloadsDirectory() async {
+    if (Platform.isAndroid) {
+      return Directory('/storage/emulated/0/Download');
+    } else if (Platform.isIOS) {
+      return await getApplicationDocumentsDirectory();
+    }
+    return null;
+  }
 
   onOpenWebPageClick() {}
 
