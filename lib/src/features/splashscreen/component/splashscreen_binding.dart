1d94b8385f374b93612d5ee91dbb379dff647222
Dias Utsman <utsmand91@gmail.com>
2024-05-30 07:12:55 +0700

    "App should go straight to dashboard page if user is logged in (or Splash -> dashboard if you have done beginner splash page challenge)" completed

--- lib/src/features/splashscreen/component/splashscreen_binding.dart
+++ lib/src/features/splashscreen/component/splashscreen_binding.dart
@@ -1,13 +1,20 @@
 import 'package:dio/dio.dart';
-import 'package:entrance_test/src/repositories/user_repository.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
+import '../../../repositories/user_repository.dart';
 import 'splashscreen_controller.dart';
 
 class SplashscreenBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(SplashscreenController());
+    Get.put(UserRepository(
+      client: Get.find<Dio>(),
+      local: Get.find<GetStorage>(),
+    ));
+
+    Get.put(SplashscreenController(
+      userRepository: Get.find<UserRepository>(),
+    ));
   }
 }
--- lib/src/features/splashscreen/component/splashscreen_controller.dart
+++ lib/src/features/splashscreen/component/splashscreen_controller.dart
@@ -1,16 +1,22 @@
 import 'package:entrance_test/app/routes/route_name.dart';
+import 'package:entrance_test/src/repositories/user_repository.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashscreenController extends GetxController {
+  final UserRepository _userRepository;
+
+  SplashscreenController({
+    required UserRepository userRepository,
+  }) : _userRepository = userRepository;
   @override
   void onInit() {
     super.onInit();
 
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
-    navigateToLogin();
+    navigateToNextPage();
   }
 
   @override
@@ -22,13 +28,20 @@ class SplashscreenController extends GetxController {
     super.onClose();
   }
 
-  void navigateToLogin() {
-    Future.delayed(
+  void navigateToNextPage() async {
+    await Future.delayed(
       const Duration(seconds: 3),
-      () {
-        Get.offAllNamed(RouteName.login);
-      },
     );
+
+    try {
+      //* Get user using current token
+      await _userRepository.getUser();
+      //* If no error occured then go to dashboard
+      Get.offAllNamed(RouteName.dashboard);
+    } catch (e) {
+      //* Else, go to login
+      Get.offAllNamed(RouteName.login);
+    }
   }
 
   Future<String> loadVersion() async {
